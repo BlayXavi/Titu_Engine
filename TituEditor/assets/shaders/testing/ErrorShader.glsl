999cd345ea176f69b5a1367911ba39f8e71e0ddb
BlayXavi <blay.xavi@gmail.com>
2022-02-12 20:09:36 +0100

    Write to IDs buffer on ShaderError.glsls

--- TituEditor/assets/shaders/testing/ErrorShader.glsl
+++ TituEditor/assets/shaders/testing/ErrorShader.glsl
@@ -3,6 +3,9 @@
 #version 450 core
 			
 layout(location  = 0) in vec3 a_Position;
+layout(location  = 5) in int a_EntityID;
+
+layout (location = 0) out flat int v_EntityID;
 
 layout(std140, binding = 0) uniform Camera
 {
@@ -12,6 +15,8 @@ layout(std140, binding = 0) uniform Camera
 void main()
 {
 	gl_Position = u_ModelViewProjectionMatrix * vec4(a_Position, 1.0);
+
+	v_EntityID = a_EntityID;
 }
 
 #type fragment
@@ -19,8 +24,12 @@ void main()
 #version 450 core
 			
 layout(location = 0) out vec4 color;
+layout(location = 1) out int colorId;
+
+layout (location = 0) in flat int v_EntityID;
 
 void main()
 {
 	color = vec4(255.0f, 0.0f, 255.0f, 1.0f);
+	colorId = v_EntityID;
 }
